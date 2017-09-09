cdd21b5152fb88e98dd9b7b8d0aee940b662c431
Rachel <rachel.green89@gmail.com>
2017-09-09 20:10:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ function logWhisper(string){
   console.log(string);
 
 }
+
+function sayHiToGrandma(string){
+  if(string.toLowerCase() === string) {
+    return `I can't hear you!`
+  } else if (string.UpperCase() === string) {
+    return `YES INDEED!`
+  } else if (string === "I love you, Grandma.") {
+    return `I love you, too.`
+  }
+}
